fc91ff9121659fdd18fde724b9f2c32923fecbe8
nosorae <nosorae@naver.com>
2021-07-07 20:44:02 +0900

    백준 브루트포스 부분수열의 합 해결, 종이조각은 아직 미해결(Main에 있다.)

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -1,73 +1,82 @@
 
 /*
- * BOJ 11723 집합
- * 비트마스크 연습차 해보는 집합 문제
- * 숫자 제한이 1~20이라서 비트마스크로 가능한 것이다. 32개 넘으면 해쉬셋을 쓰자
+ * BOJ 일단 한자리로 더하는 것보다 여러자리로 만드는 게 크게만들 것 같은데 반례는 0이 들어간 경우 생긴다.
+ * 그래서 범위도 1<= N, M <= 4로 작으니 브루트포스로 해결하려고 한다.
+ * 비트마스크로 모든 경우의 수를 다 해보는데 비트마스크는 1차원이므로  
+ * 1차원 배열의 인덱스를 2차원 배열화해서 문제를 풀어준다.
+ * 각 케이스마다 완성된 종이조각에 대한 숫자 파싱이 필요하다.
  */
 import java.util.*;
 import java.io.*;
 
 public class Main {
+	static int n;
+	static int m;
 	public static void main(String[] args) throws IOException {	
 		BufferedReader br = new  BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int n = Integer.parseInt(br.readLine());
-
-		int set = 0;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		int[][] paper = new int[n][m];
 		for(int i = 0; i < n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			String cmd = st.nextToken();
-			switch(cmd) {
-			case "add":
-				set = add(set, Integer.parseInt(st.nextToken()));
-				break;
-			case "remove":
-				set = remove(set, Integer.parseInt(st.nextToken()));
-				break;
-			case "check":
-				if(check(set, Integer.parseInt(st.nextToken())))
-					bw.write(1+"\n");
-				else
-					bw.write(0+"\n");
-				break;
-			case "toggle":
-				set = toggle(set, Integer.parseInt(st.nextToken()));
-				break;
-			case "all":
-				set = (1 << 21) - 1;
-				break;
-			case "empty":
-				set = 0;
+			String input = br.readLine();
+			for(int j = 0; j < m; j++) {
+				paper[i][j] = input.charAt(j) - '0';
 			}
 		}
-		br.close();
-		bw.flush(); bw.close(); 
 		
-
-
-
-	}
-	static int add(int n, int x) {
-		return (n | (1 << x));
+		int max = 0;
+		
+		for(int i = 0; i < 1<<(n*m); i++) {
+			boolean[][] check = new boolean[n][m]; // true면 해당 칸은 가로다
+			for(int j = 0; j < n*m; j++) {
+				if((i & (1 << j)) != 0) {
+					check[j/n][j%n] = true;
+				}
+			}
+			int localAnswer = sumAllValue(paper, check);
+			if(max < localAnswer)
+				max = localAnswer;
+		}
+		
+		System.out.println(max);
+		
 		
 	}
-	static int remove(int n, int x) {
-		if(check(n, x))
-			return (n - (1 << x));
-		else
-			return n;
+	static int sumAllValue(int[][] paper, boolean[][] check) {
+		int result = 0;
+		//가로
+		for(int i = 0; i < n; i++) {
+			int cur = 0;
+			for(int j = 0; j < m; j++) {
+				if(check[i][j]) {
+					cur *= 10;
+					cur += paper[i][j];
+				}
+				else {
+					result += cur;
+					cur = 0;
+				}
+			}
+			result += cur;
+		}
+		//세로 
+		for(int i = 0; i < m; i++) {
+			int cur = 0;
+			for(int j = 0; j < n; j++) {
+				if(check[j][i]) {
+					cur *= 10;
+					cur += paper[j][i];
+				}
+				else {
+					result += cur;
+					cur =0;
+				}
+			}
+			result += cur;
+		}
+		return result;
 	}
-	static boolean check(int n, int x) {
-		if((n & (1 << x)) != 0)
-			return true;
-		else
-			return false;
-	}
-	static int toggle(int n, int x) {
-		n = (n ^ (1 << x));
-		return n;
-	} 
+
 }
-
-
 
---
+++ Algorithm/src/bruteforce/BOJ_1182.java
@@ -0,0 +1,77 @@
+package bruteforce;
+
+/*
+ * BOJ 부분수열의 합 1182 https://www.acmicpc.net/source/21358795
+ * 조회를 binaryString으로 바꿀 필요없이, 그냥  v1 & (1<<i) 이렇게 i를 0부터 원하는 길이까지 조회하는 게 더 빠르다.
+ */
+import java.util.*;
+import java.io.*;
+
+public class BOJ_1182 {
+	public static void main(String[] args) throws IOException {	
+		BufferedReader br = new  BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int goal = Integer.parseInt(st.nextToken());
+		int[] input = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+		System.out.println(findAllSum(input, goal));
+	} 
+	static int findAllSum(int[] arr, int goal) {
+		int result = 0;
+		// 부분수열의 크기가 양수라고 했으니 1부터 시작해야한다.
+		for(int i = 1; i < 1 << arr.length; i++) {
+			String s = Integer.toBinaryString(i);
+			int sum = 0;
+			for(int j = s.length()-1; j >= 0; j--) {
+				if(s.charAt(j) == '1') {
+					sum += arr[s.length()-1 - j];
+				}
+			}
+			if(sum == goal)
+				result++;
+		}
+		return result;
+	}
+}
+
+
+
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//import java.util.StringTokenizer;
+//public class Main {
+//	public static void main(String args[]) throws Exception {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+//		int n = Integer.parseInt(st.nextToken());
+//		int m = Integer.parseInt(st.nextToken());
+//		st = new StringTokenizer(br.readLine());
+//		int[] arr = new int[n];
+//		for(int i = 0; i < n; i++) {
+//			arr[i] = Integer.parseInt(st.nextToken());
+//		}
+//		int sum = 0;
+//		int ans = 0;
+//		for(int i = 1; i <(1<<n); i++) {
+//			sum = 0;
+//			for(int j = 0; j < n; j++) {
+//				if((i &(1<<j)) != 0) {
+//					sum += arr[j];
+//				}
+//			}
+//			if(sum == m) {
+//				//System.out.println(i);
+//				ans++;
+//			}
+//		}
+//		System.out.println(ans);
+//	}
+//
+//}
+
