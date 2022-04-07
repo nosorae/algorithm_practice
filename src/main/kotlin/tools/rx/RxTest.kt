4c82d091e88003a8a019a03dcca5ad9b5bd5015c
sorea.no <nosorae@naver.com>
2022-04-07 23:55:34 +0900

    Rx: Observable, Observer, Emitter, create, subscibe 개념 학습

--- src/main/kotlin/tools/rx/RxTest.kt
+++ src/main/kotlin/tools/rx/RxTest.kt
@@ -8,4 +8,18 @@ fun main() {
         .just("Hello?", "RxJava!?") // 생성 연산자
         .map { it.dropLast(1) } // 변환 연산자 : 데이터의 가공 (뒷자리 하나 제거)
         .subscribe { println(it) } // 소비자 : 데이터를 받아서 처리 (println)
+    println()
+
+    /**
+     * Observable : 데이터 순차적으로 발행 (onNext), 데이터 발행 끝났음을 알림 (onComplete), 오류발생 알림 (onError) to Observer
+     * Observer : Observable 을 구독함
+     * Emitter : Observable create 시 이것을 이용하여 직접 아이템을 발행하고 완료 및 오류의 알림을 직접 설정할 수 있음.
+     */
+    val source: Observable<String> = Observable.create { emitter ->
+        emitter.onNext("Hello")
+        emitter.onNext("World")
+        emitter.onNext("SoRae")
+        emitter.onComplete()
+    }
+    source.subscribe { println(it) }
 }
