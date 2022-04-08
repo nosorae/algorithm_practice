94b37460b24433392cb6dae0464ed1ea6e45343b
sorea.no <nosorae@naver.com>
2022-04-08 20:05:44 +0900

    Rx: Emitter - onComplete, onError 처리 학습

--- src/main/kotlin/tools/rx/RxTest.kt
+++ src/main/kotlin/tools/rx/RxTest.kt
@@ -15,11 +15,38 @@ fun main() {
      * Observer : Observable 을 구독함
      * Emitter : Observable create 시 이것을 이용하여 직접 아이템을 발행하고 완료 및 오류의 알림을 직접 설정할 수 있음.
      */
-    val source: Observable<String> = Observable.create { emitter ->
+    var source: Observable<String> = Observable.create { emitter ->
         emitter.onNext("Hello")
         emitter.onNext("World")
         emitter.onNext("SoRae")
         emitter.onComplete()
     }
     source.subscribe { println(it) }
+
+    /**
+     * onComplete 호출 후에는 아이템이 추가로 발행되더라도 구독자는 데이터 통지받지 못함
+     */
+    source = Observable.create { emt ->
+        emt.onNext("Hello")
+        emt.onComplete()
+        emt.onNext("World")
+    }
+    source.subscribe { println(it) }
+
+    /**
+     * 에러발생 시 후에 추가 발행되는 아이템은 통지받지 못하고, Emittere 의 onError(Throwable) 을 호출하고 구독자는 이를 처리한다.
+     */
+    source = Observable.create { emt ->
+        emt.onNext("Hello")
+        emt.onError(Throwable())
+        emt.onNext("World")
+    }
+    source.subscribe(
+        { item ->
+            println(item)
+        },
+        { throwable ->
+            println("Error")
+        }
+    )
 }
