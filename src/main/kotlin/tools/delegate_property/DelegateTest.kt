2dbb26e31752036f0ffd4ca97a7c1519cccd06d4
nosorae <shthfo@gmail.com>
2023-01-22 02:31:15 +0900

    study: 위임 프로퍼티 사용 예시 - 커스텀 위임객체

--- src/main/kotlin/tools/delegate_property/DelegateTest.kt
+++ src/main/kotlin/tools/delegate_property/DelegateTest.kt
@@ -1,7 +1,5 @@
 package tools.delegate_property
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import kotlin.reflect.KProperty
@@ -17,10 +15,40 @@ class LazyTest {
     }
 }
 
-fun main() = runBlocking {
-    val lazyTest = LazyTest().test
-    repeat(3) {
-        println(lazyTest)
+data class Engine(val name: String, val old: Int)
+
+class EngineDelegate(private val initEngine: Engine) {
+    private var _engine: Engine? = null
+
+    operator fun getValue(car: Car, property: KProperty<*>): Engine {
+        println("엔진 대신 가져오는 중...")
+        return _engine ?: run {
+            _engine = initEngine
+            initEngine
+        }
     }
 
+    operator fun setValue(car: Car, property: KProperty<*>, newEngine: Engine) {
+        println("새로운 엔진으로 교체중...")
+        _engine = newEngine
+    }
 }
+
+class Car {
+    var engine: Engine by EngineDelegate(Engine("구형 엔진", 9))
+}
+
+fun main() {
+//    val lazyTest = LazyTest().test
+//    repeat(3) {
+//        println(lazyTest)
+//    }
+
+    Car().run {
+        println(engine)
+        println(engine)
+        engine = Engine("신형 엔진", 0)
+        println(engine)
+    }
+}
+
