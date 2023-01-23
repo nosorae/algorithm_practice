97ee4a3e61bccefdeb731de69a5ec85cbc63d1ed
nosorae <shthfo@gmail.com>
2023-01-24 02:10:28 +0900

    study: 함수형 인터페이스 예시 추가

--- src/main/kotlin/tools/lambda/MemberReference.kt
+++ src/main/kotlin/tools/lambda/MemberReference.kt
@@ -1,5 +1,11 @@
 package tools.lambda
 
+import kotlin.reflect.KClass
+import kotlin.reflect.KFunction1
+import kotlin.reflect.KFunction2
+import kotlin.reflect.KProperty1
+import kotlin.reflect.KProperty
+
 data class Car(val price: Int) {
     fun isExpensive(): Boolean {
         // 비싼지 안비싼지 복잡한 연산이 필요하다고 가정하자...
@@ -17,6 +23,12 @@ data class CarEntity(val price: Int)
 val Car.doublePrice: Int get() = price * 2
 
 fun externalPrint() = println("아무말~")
+
+fun sum(x: Int, y: Int) = x + y
+val kFunction: KFunction2<Int, Int, Int> = ::sum
+val carFunction: KFunction1<Car, Boolean> = Car::isExpensive
+val carProperty: KProperty1<Car, Int> = Car::price
+val carR: (Car) -> Boolean = { car -> true }
 
 fun main() {
     val carCreator = ::Car // 생성자 참조
@@ -40,5 +52,10 @@ fun main() {
         }
 
     run(::externalPrint) // 최상위 선언 참조
+
+
 }
 
+interface Function2<in P1, in P2, in P3, out R> {
+    operator fun invoke(p1: P1, p2: P2, p3: P3): R
+}
