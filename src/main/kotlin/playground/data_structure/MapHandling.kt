4d989d1f2e3e88eba833ce25a5e703d2d4c3d9a0
yessorae <yessorae@gmail.com>
2023-12-13 05:31:17 +0900

    map 핸들링 연습

--- src/main/kotlin/playground/data_structure/MapHandling.kt
+++ src/main/kotlin/playground/data_structure/MapHandling.kt
@@ -1,12 +1,12 @@
-package playground.map
+package playground.data_structure
 
 
 fun main() {
 
     // 기본적인 초기화와 set, get
-    val map = mutableMapOf<String, Int>()
-    map.put("thfo", 10)
-    map.set("thfo", 10)
+    val map = mutableMapOf<String, Int>() // LinkedHashMap
+    map["thfo"] = 10
+    map["thfo"] = 10
     map["thfo"] = 10
 
 
@@ -19,7 +19,7 @@ fun main() {
 
     // 다른 Iterable 로 부터 groupBy 로 카운트하고 변형해보기
     val list = listOf(1, 2, 3, 1, 1, 1, 2, 7, 7, 4)
-    val groupedList = list.groupBy { // Map<T, List<T>> 반환
+    val groupedList = list.groupBy {
         it
     }
     val gList = list.groupBy(
@@ -31,9 +31,9 @@ fun main() {
         }
     )
     println(groupedList.toString())
-    println(groupedList.forEach { k, v -> println(v.size) })
+    println(groupedList.forEach { (k, v) -> println("$k = " + v.size) })
     println(gList.toString())
-    println(gList.forEach { k, v -> println(v.size) })
+    println(gList.forEach { (k, v) -> println("$k = " + v.size) })
 
 
     // LinkedHashMap 이 추가 순서를 지켜준다는 점을 이용해서 바로 value 들만 뽑아서 IntArray 로 반환환
