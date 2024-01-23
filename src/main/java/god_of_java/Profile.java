4da097167ddbf41ada1ddde0cde0aa38c8e3b9e3
yessorae <yessorae@gmail.com>
2024-01-23 22:17:15 +0900

    GodOfJava Vol1. 2장 연습문제

---
+++ src/main/java/god_of_java/Profile.java
@@ -0,0 +1,14 @@
+package god_of_java;
+
+// GodOfJava Vol1. 2장
+/*
+ 커맨드 명령어 사용
+ yessorae@nosoraeui-MacBookPro god_of_java % javac Profile.java <- 컴파일, .class 생성
+ yessorae@nosoraeui-MacBookPro god_of_java % java Profile.java <- 실행
+ */
+public class Profile {
+    public static void main(String[] args) {
+        System.out.println("My name is No So Rae");
+        System.out.println("My age is 28");
+    }
+}
