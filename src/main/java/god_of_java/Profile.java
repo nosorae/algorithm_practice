4484e5a6c6c2e9b628b489875df736393d608e85
yessorae <yessorae@gmail.com>
2024-01-24 21:37:34 +0900

    GodOfJava Vol1. 3장 연습문제

--- src/main/java/god_of_java/Profile.java
+++ src/main/java/god_of_java/Profile.java
@@ -1,14 +1,37 @@
 package god_of_java;
 
-// GodOfJava Vol1. 2장
+// GodOfJava Vol1. 2장, 3장
 /*
  커맨드 명령어 사용
  yessorae@nosoraeui-MacBookPro god_of_java % javac Profile.java <- 컴파일, .class 생성
  yessorae@nosoraeui-MacBookPro god_of_java % java Profile.java <- 실행
  */
 public class Profile {
+    String name;
+    int age;
+
     public static void main(String[] args) {
-        System.out.println("My name is No So Rae");
-        System.out.println("My age is 28");
+        Profile profile = new Profile();
+        profile.setName("No So Rae");
+        profile.setAge(28);
+
+        profile.printName();
+        profile.printAge();
+    }
+
+    public void setName(String str) {
+        name = str;
+    }
+
+    public void setAge(int val) {
+        age = val;
+    }
+
+    public void printName() {
+        System.out.println(String.format("My name is %s", name));
+    }
+
+    public void printAge() {
+        System.out.println(String.format("My age is %d", age));
     }
 }
