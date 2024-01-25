0b5ba6a200c7263493c5018c6a327933752f9c92
yessorae <yessorae@gmail.com>
2024-01-25 22:05:39 +0900

    GodOfJava Vol1. 4장 연습문제

---
+++ src/main/java/god_of_java/ProfilePrint.java
@@ -0,0 +1,42 @@
+package god_of_java;
+
+// GodOfJava Vol1. 4장
+public class ProfilePrint {
+    byte age;
+    String name;
+    boolean isMarried;
+
+    public static void main(String[] args) {
+        ProfilePrint profilePrint = new ProfilePrint();
+        profilePrint.setAge((byte)29);
+        profilePrint.setName("SO RAE");
+        profilePrint.setMarried(true);
+        System.out.println(profilePrint.getAge());
+        System.out.println(profilePrint.getName());
+        System.out.println(profilePrint.isMarried());
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setMarried(boolean flag) {
+        isMarried = flag;
+    }
+
+    public boolean isMarried() {
+        return isMarried;
+    }
+}
