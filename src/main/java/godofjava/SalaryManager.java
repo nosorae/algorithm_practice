9bd5f5934b6ef1b705e0ce4fd7ecce42797d90e4
yessorae <yessorae@gmail.com>
2024-01-28 21:33:32 +0900

    GodOfJava Vol1. 5장 연습문제

---
+++ src/main/java/godofjava/SalaryManager.java
@@ -0,0 +1,41 @@
+package godofjava;
+
+// GodOfJava Vol1. 5장
+public class SalaryManager {
+    private final double salaryTax = 12.5;
+    private final double nationalPension = 8.1;
+    private final double healthInsurance = 13.5;
+
+    public static void main(String[] args) {
+        SalaryManager salaryManager = new SalaryManager();
+        System.out.println(salaryManager.getMonthlySalary(90_000_000));
+    }
+
+    public double getMonthlySalary(int yearlySalary) {
+        double monthlySalary = yearlySalary / 12.0;
+        double salaryTaxResult = calculateTax(monthlySalary);
+        double nationalPensionResult = calculateNationalPension(monthlySalary);
+        double healthInsuranceResult = calculateHealthInsurance(monthlySalary);
+        double totalDeduction = salaryTaxResult + nationalPensionResult + healthInsuranceResult;
+        monthlySalary -= totalDeduction;
+        return monthlySalary;
+    }
+
+    public double calculateTax(double monthlySalary) {
+        double tax = monthlySalary * (salaryTax / 100);
+        System.out.println("근로소득세 : " + tax);
+        return tax;
+    }
+
+    public double calculateNationalPension(double monthlySalary) {
+        double tax  = monthlySalary * (nationalPension / 100);
+        System.out.println("국민연금 공제 : " + tax);
+        return tax;
+    }
+
+    public double calculateHealthInsurance(double monthlySalary) {
+        double tax =  monthlySalary * (healthInsurance / 100);
+        System.out.println("건강 보험료 공제 : " +  tax);
+        return tax;
+    }
+}
