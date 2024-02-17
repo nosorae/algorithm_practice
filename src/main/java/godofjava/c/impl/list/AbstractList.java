c9568db595d53ae4f3cb4c2b9d8307bc3dca25f8
yessorae <yessorae@gmail.com>
2024-02-17 22:16:14 +0900

    GodOfJava Vol1. 13장 연습문제

---
+++ src/main/java/godofjava/c/impl/list/AbstractList.java
@@ -0,0 +1,7 @@
+package godofjava.c.impl.list;
+
+// GodOfJava Vol1. 13장 연습문제 1
+public abstract class AbstractList implements List {
+    public abstract void clear();
+
+}
---
+++ src/main/java/godofjava/c/impl/list/List.java
@@ -0,0 +1,12 @@
+package godofjava.c.impl.list;
+
+// GodOfJava Vol1. 13장 연습문제 1
+public interface List {
+    public void add();
+
+    public void update(int index, Object value);
+
+    public void remove(int index);
+
+    public void get(int index);
+}
