389be45be290987144be00480f83fc4baa71b527
yessorae <yessorae@gmail.com>
2024-02-20 22:35:51 +0900

    GodOfJava Vol1. 16장 연습문제 추가 확인 추석 추가

--- src/main/java/godofjava/c/inner/practice/MyPage.java
+++ src/main/java/godofjava/c/inner/practice/MyPage.java
@@ -1,6 +1,8 @@
 package godofjava.c.inner.practice;
 
 // GodOfJava Vol1. 16장 연습문제
+// 컴파일해서 실행해보면 익명클래스는 MyPage$1.class 로 나오는 것을 확인할 수 있다.
+// 여러 파일 한 번에 컴파일은 https://www.delftstack.com/ko/howto/java/compile-multiple-java-files/ 참고
 public class MyPage {
     static InputBox input;
     public static void main(String[] args) {
